18791f02bf731a913f26f218063c6dd2da09b23d
kahtanov <Zop859@mail.ru>
2020-11-12 00:31:16 +0300

    вынос окон в отдельные контролеры

---
+++ UserControls/AddTeacherControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiplomaStorage.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для AddTeacherControl.xaml
+    /// </summary>
+    public partial class AddTeacherControl : UserControl
+    {
+        public AddTeacherControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
