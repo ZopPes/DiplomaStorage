db1675da5a3f05f9a018d746c9b611a547b2a49d
kahtanov <Zop859@mail.ru>
2020-11-11 20:09:02 +0300

    добавить класс для отрывающихся окон

---
+++ UserControls/MyInterTabClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DiplomaStorage.UserControls;
+using Dragablz;
+
+namespace DiplomaStorage
+{
+    public class MyInterTabClient :  Tab, IInterTabClient
+    {
+
+
+        public INewTabHost<Window> GetNewHost(IInterTabClient interTabClient, object partition, TabablzControl source)
+        {
+            var View = new testDragablz();
+            
+            return new NewTabHost<Window>(View, View.TabCon);
+        }
+
+        public TabEmptiedResponse TabEmptiedHandler(TabablzControl tabControl, Window window)
+        {
+            return TabEmptiedResponse.CloseWindowOrLayoutBranch;
+        }
+    }
+}
