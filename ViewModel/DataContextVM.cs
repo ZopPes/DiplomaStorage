1243a68acca19d0d524f0777d1cf8b751d42c3ad
kahtanov <Zop859@mail.ru>
2020-11-15 16:49:16 +0300

    создал и реализовал класс

---
+++ ViewModel/DataContextVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DiplomaStorage
+{
+    class DataContextVM : DiplomaStorageDataContext, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string Property = null)
+        {
+            if (!Equals(field, value) || value == null) return false;
+
+            field = value;
+            OnPropertyChanged();
+            return true;
+        }
+    }
+}
